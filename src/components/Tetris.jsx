1227771cbb6ec1337f55af2742e50c2aceeaf016
tamaramunoz <tami.skt@gmail.com>
2020-06-29 13:36:17 -0400

    game over function

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { createStage } from '../gameHelpers'
+import { createStage, checkCollision } from '../gameHelpers'
 
 // styled components
 import { StyledTetrisWrapper, StyledTetris } from '../components/styles/StyledTetris'
@@ -23,24 +23,36 @@ const Tetris = () => {
     const [stage, setStage] = useStage(player, resetPlayer);
 
     const movePlayer = dir => {
-        updatePlayerPos({
-            x: dir,
-            y: 0
-        })
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({
+                x: dir,
+                y: 0
+            });
+        }
     }
 
     const startGame = () => {
         // reset everything
         setStage(createStage())
-        resetPlayer();
+        resetPlayer()
+        setGameOver(false)
     }
 
     const drop = () => {
-        updatePlayerPos({
-            x: 0,
-            y: 1, 
-            collided: false
-        })
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({
+                x: 0,
+                y: 1,
+                collided: false
+            })
+        }else{
+            // game over
+            if(player.pos.y < 1){
+                setGameOver(true)
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
     }
 
     const dropPlayer = () => {
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,33 +1,39 @@
+
 import { useState, useEffect } from 'react'
 import { createStage } from '../gameHelpers'
 
+
 export const useStage = (player, resetPlayer) => {
-    const [stage, setStage] = useState(createStage())
+  const [stage, setStage] = useState(createStage())
 
-    useEffect(() => {
-        const updateStage = prevStage => {
-            // first flush the stage
-            const newStage = prevStage.map(row => 
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
-            );
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // first flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
 
-            // the draw the tetromino
-            player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if(value !== 0){
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`
-                        ]
-                    }
-                })
-            })
-            return newStage;
-        }
+      // draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+      // check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
-        setStage(prev => updateStage(prev))
-        
-    }, [player])
+      return newStage;
+    };
 
-    return [stage, setStage]
-}
+    setStage(prev => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage];
+};
