62d42c2e4e3712ec9244e97fe803a851823e6fd9
tamaramunoz <tami.skt@gmail.com>
2020-06-28 20:09:49 -0400

    start button with its function and showing random tetromino

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import { StyledStartButton } from './styles/StyledStartButton'
 
-const StartButton = ({ callback }) => {
-    return (
-        <StyledStartButton onclick={callback} >
-            Start Game
-        </StyledStartButton>
-    )
-}
-
-export default StartButton
+const StartButton = ({ callback }) => (
+    <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
+  );
+  
+  export default StartButton;
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { createStage } from '../gameHelpers'
+
 // styled components
 import { StyledTetrisWrapper, StyledTetris } from '../components/styles/StyledTetris'
 
@@ -17,15 +19,49 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
 
-    const [player] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
 
+    const movePlayer = dir => {
+        updatePlayerPos({
+            x: dir,
+            y: 0
+        })
+    }
 
+    const startGame = () => {
+        // reset everything
+        setStage(createStage())
+        resetPlayer();
+    }
 
-    console.log('re-render');
+    const drop = () => {
+        updatePlayerPos({
+            x: 0,
+            y: 1, 
+            collided: false
+        })
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            }
+        }
+    }
+
 
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
@@ -37,7 +73,7 @@ const Tetris = () => {
                                 <Display text="Level" />
                             </div>
                         )}
-                    <StartButton />
+                    <StartButton callback={startGame} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
